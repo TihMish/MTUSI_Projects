debafb9025ae4bbaac990fea3c5a5d27a4af50d5
Tihaya_mishka <yashkoldina.na@gmail.com>
2026-09-25 10:08:04 +0300

    Q5, main.py with input and type conversion (name, surname, age, height)

--- python_lab_02/main.py
+++ python_lab_02/main.py
@@ -1,16 +1,33 @@
 # Задание 3
 
-print("Hello, World!")
+# print("Hello, World!")
+
 
 # Задание 4
 
-n4, a4, h4, s4 = "Настя", 18, 165, True
+# n4, a4, h4, s4 = "Настя", 18, 165, True
 
-print(n4, a4, h4, s4, sep='\n')
-print(type(n4), type(a4), type(h4), type(s4), sep='\n')
+# print(n4, a4, h4, s4, sep='\n')
+# print(type(n4), type(a4), type(h4), type(s4), sep='\n')
 
-a4, h4 = 19, 168
-print(a4, h4, sep='\n')
+# a4, h4 = 19, 168
+# print(a4, h4, sep='\n')
+
+
+
+# Задание 5
+
+print("Задание 5. Введите по очереди: имя, фамилию, возраст, рост")
+n5, f5, a5 = input("Имя: "), input("Фамилия: "), input("Возраст: ")
+print(n5, f5, a5, sep='\n')
+
+a5 = int(a5)
+h5 = float(input("Рост: "))
+
+print(a5, type(a5), h5, type(h5), sep='\n')
+
+
+
 
 
 
